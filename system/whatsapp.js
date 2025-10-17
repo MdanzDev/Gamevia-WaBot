513c54042057cabfa3acac70f212e7a50f3a80ed
MdanzDev <nightmaremoon297@gmail.com>
2025-10-17 20:59:10 +0800

    Update whatsapp.js

--- system/whatsapp.js
+++ system/whatsapp.js
@@ -1553,7 +1553,7 @@ ${metrics.popularCommands.map(cmd =>
                 }
 
                 const gameButtons = categoryGames.map(game => ({
-                    buttonId: `select-${game.slug}`,
+                    buttonId: `.select-${game.slug}`,
                     buttonText: { displayText: game.name },
                     type: 1
                 }));
@@ -1627,7 +1627,7 @@ ${metrics.popularCommands.map(cmd =>
             
             let gameName = slug;
             try {
-                const gamesData = await cachedApiCall('check_games.php');
+                const gamesData = await ApiCall('check_games.php');
                 if (gamesData.success) {
                     const game = gamesData.games.find(g => g.slug === slug);
                     if (game) gameName = game.name;
@@ -1751,7 +1751,7 @@ ${metrics.popularCommands.map(cmd =>
             let productName = session.productCode;
             let price = "0.00";
             try {
-                const productData = await cachedApiCall('get_products.php', { slug: session.gameSlug });
+                const productData = await ApiCall('get_products.php', { slug: session.gameSlug });
                 const product = productData.products?.find(p => p.srv_code === session.productCode);
                 if (product) {
                     productName = product.name;
